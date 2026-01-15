0fd2d3dd77484482d5330ace78cf69e9498f55e3
TitanSlayer256 <106034429+TitanSlayer256@users.noreply.github.com>
2026-01-15 16:54:03 -0700

    Update Prep3 solution

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(101); // Generates a number between 0 and 99
+
+        Console.WriteLine("Guess a Number between 1 and 100.");
+
+        Console.Write("What is your guess?");
+        int guess = Convert.ToInt32(Console.ReadLine());;
+
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("Wow you got it on the first try!");
+        }
+        while (guess != magicNumber)
+        {  
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            } else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.Write("What is your guess?");
+            guess = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Bingo! You got it!");
+
+
     }
 }
