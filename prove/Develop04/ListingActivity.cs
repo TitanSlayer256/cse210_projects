cba38a7aec3e4e814f67e7e48b45566ce55aefc8
TitanSlayer256 <106034429+TitanSlayer256@users.noreply.github.com>
2026-02-26 17:44:34 -0700

    Add Develop04 activity classes and program implementation

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,34 @@
+public class ListingActivity : Activity
+{
+    private List<string> _prompts = new List<string> {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?"
+    };
+
+    public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life.") {}
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        Random random = new Random();
+        
+        Console.WriteLine("\nList as many items as you can to the following prompt:");
+        Console.WriteLine($" --- {_prompts[random.Next(_prompts.Count)]} ---");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        int count = 0;
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"You listed {count} items!");
+        DisplayEndingMessage();
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string choice = ""; //initialize the variable for the user's choice
+        while (choice != "4") //Logic to loop menu until the user selects 4 to quit
+        {
+            //Continuously creates the menu each time the user completes an activity until they select 4 to quit
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflection activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            //logic to run the activity based on the user's choice
+            if (choice == "1") {
+                BreathingActivity ba = new BreathingActivity();
+                ba.Run();
+            }
+            else if (choice == "2") {
+                ReflectionActivity ra = new ReflectionActivity();
+                ra.Run();
+            }
+            else if (choice == "3") {
+                ListingActivity la = new ListingActivity();
+                la.Run();
+            }
+        }
     }
 }
